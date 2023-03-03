832cb680a4e339166977542a240ce3fab7f0e8c6
acappleman <112197155+acappleman@users.noreply.github.com>
2023-03-03 10:49:46 -0500

    Adding some components

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer(props) {
+    return <footer>
+        <section class="contact-me" id="contact">
+          <h3>Contact Me</h3>
+          <div>
+            <address>
+              <a href="tel:+14074689187">407.468.9187</a>
+              <a href="mailto:amanda.cappleman@gmail.com"
+                >amanda.cappleman@gmail.com</a
+              >
+              <a href="https://github.com/acappleman">GitHub</a>
+              <a href="https://www.linkedin.com/in/amanda-cappleman-ba8b60250/"
+                >Linkedin</a
+              >
+            </address>
+          </div>
+        </section>
+    </footer>;
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer(props) {
+    return <header>
+    <h1>Amanda Cappleman</h1>
+    <nav>
+      <a href="#about">About Me</a>
+      <a href="#work">Work</a>
+      <a href="#contact">Contact Me</a>
+      <a href="https://www.linkedin.com/in/amanda-cappleman-ba8b60250/"
+        >Resume</a
+      >
+    </nav>
+  </header>;
+}
+
+export default Footer;
